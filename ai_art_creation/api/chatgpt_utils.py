1e3894a643f88855d8f6b7285204e35ea841f90e
trentleslie <trentleslie@gmail.com>
2023-05-09 09:49:19 -0700

    redoing approach to csv

--- ai_art_creation/api/chatgpt_utils.py
+++ ai_art_creation/api/chatgpt_utils.py
@@ -166,6 +166,37 @@ def generate_ongoing_tshirt_df(valid_preprompt_csv_df):
     
     return ongoing_tshirt_df
 
+
+
+def get_prompt(preprompt):
+    
+    # Set your OpenAI API key
+    openai.api_key = api_key
+
+    #build messages payload
+    messages = [
+        {"role": "system", "content": '''You are a world class Dall-E prompt engineer and expert AI language model.
+                                        '''},
+        #{"role": "user", "content": "Dall-E prompt"},
+        #{"role": "assistant", "content": "On what topic?"},
+        {"role": "user", "content": f"Please provide 1 dall-e prompt in triple backticks (```prompt```) related to {preprompt}"},
+    ]
+    
+    #call the ChatCompletion end
+    response = openai.ChatCompletion.create(
+            model = chatgpt_model,
+            messages=messages,
+            temperature = 0.8,
+            top_p = 1, 
+            presence_penalty = 0.5,
+            frequency_penalty = 0.4            
+        )
+
+    # Extract the generated text from the API response
+    generated_text = (response['choices'][0]['message']['content'])
+
+    return generated_text
+
 def get_title(prompt):
     
     # Set your OpenAI API key
@@ -180,16 +211,16 @@ def get_title(prompt):
                                             If you are unsure of how to generate the title based on the description, please use a title related to AI, artificial intelligence, deep learning, digital art, digital art design, or any adjectives or nouns that are included in or related to the description.
                                             Please write a search engine optimized title less than 50 characters of the following digital art design description:
                                             '''},
-        {"role": "user", "content": "Descriptive title"},
-        {"role": "assistant", "content": "On what topic?"},
-        {"role": "user", "content": prompt}
+        #{"role": "user", "content": "Descriptive title"},
+        #{"role": "assistant", "content": "On what topic?"},
+        {"role": "user", "content": f'Please provide a title for the Dall-E prompt in triple backticks (```title```): {prompt}'}
     ]
     
     #call the ChatCompletion end
     response = openai.ChatCompletion.create(
             model = chatgpt_model,
             messages=messages,
-            temperature = 0.8,
+            temperature = 0.5,
             top_p = 1, 
             presence_penalty = 0.5,
             frequency_penalty = 0.4            
@@ -214,16 +245,16 @@ def get_description(prompt):
                                             Do not use first person pronouns such as "I" or "we" or "us" or "our".
                                             If you are unsure of how to generate the description based on the description, please use tags related to AI art, digital art, digital art design, or any adjectives or nouns that are included in or related to the description.
                                             Please write a search engine optimized description less than 400 characters of the following digital art design description:'''},
-        {"role": "user", "content": "Casual description"},
-        {"role": "assistant", "content": "On what topic?"},
-        {"role": "user", "content": prompt}
+        #{"role": "user", "content": "Casual description"},
+        #{"role": "assistant", "content": "On what topic?"},
+        {"role": "user", "content": f'Please provide a description for the Dall-E prompt in triple backticks (```description```): {prompt}'}
     ]
     
     #call the ChatCompletion end
     response = openai.ChatCompletion.create(
             model = chatgpt_model,
             messages=messages,
-            temperature = 0.8,
+            temperature = 0.5,
             top_p = 1, 
             presence_penalty = 0.5,
             frequency_penalty = 0.4            
@@ -250,19 +281,19 @@ def get_tags(prompt):
                                             Do not use periods.
                                             Do not start the list of tags with a label.
                                             Do not mention SEO.
-                                            Please provide only the tages separated by commas.
+                                            Please provide only the tags separated by commas.
                                             If you are unsure of how to generate the tags based on the description, please use tags related to AI, artificial intelligence, deep learning, digital art, digital art design, or any adjectives or nouns that are included in or related to the description.
                                             Please provide 25 search engine optimized tags separated by commas for the following digital art design description:'''},
-        {"role": "user", "content": "Casual description"},
-        {"role": "assistant", "content": "On what topic?"},
-        {"role": "user", "content": prompt}
+        #{"role": "user", "content": "Casual description"},
+        #{"role": "assistant", "content": "On what topic?"},
+        {"role": "user", "content": f'Please provide 25 tags, separated by commas, for the Dall-E prompt in triple backticks (```tag 1, tag2, tag3...```): {prompt}'}
     ]
     
     #call the ChatCompletion end
     response = openai.ChatCompletion.create(
             model = chatgpt_model,
             messages=messages,
-            temperature = 0.8,
+            temperature = 0.5,
             top_p = 1, 
             presence_penalty = 0.5,
             frequency_penalty = 0.4            
@@ -272,3 +303,43 @@ def get_tags(prompt):
     generated_text = (response['choices'][0]['message']['content'])
 
     return generated_text
+
+def extract_content(content):
+    start = content.find("```")
+    end = content.find("```", start + 3)
+    if start != -1 and end != -1:
+        return content[start + 3:end].strip()
+    return content.strip()
+
+def csv_generator(file_name, number_of_rows):
+    with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['prompt', 'title', 'description', 'tags']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for i in range(number_of_rows):
+            prompt = get_prompt("dna")
+            print("Successfully generated prompt: " + prompt)
+            time.sleep(3)
+            title = get_title(prompt)
+            print("Successfully generated title: " + title)
+            time.sleep(3)
+            description = get_description(prompt)
+            print("Successfully generated description: " + description)
+            time.sleep(3)
+            tags = get_tags(prompt)
+            print("Successfully generated tags: " + tags)
+            time.sleep(3)
+
+            writer.writerow({
+                'prompt': extract_content(prompt),
+                'title': extract_content(title),
+                'description': extract_content(description),
+                'tags': extract_content(tags)
+            })
+            
+            print(f'Row {i + 1} of {number_of_rows} completed')
+
+# Usage
+csv_generator('output.csv', 5)
+
